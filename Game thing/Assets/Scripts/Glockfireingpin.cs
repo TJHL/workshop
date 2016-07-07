6dfb2e76a780f10c450f0811332d43379380a6d0
TJHL <league@WTS14.attlocal.net>
2016-07-07 15:00:57 -0700

    GLOCK

---
+++ Game thing/Assets/Scripts/Glockfireingpin.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Glockfireingpin : MonoBehaviour {
+
+	public GameObject bullet;
+
+	void Update(){
+		//Creates a bullet the the desired key is pressed. 
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+			Instantiate (bullet, transform.position, bullet.transform.rotation);
+		}
+	}
+}
---
+++ Game thing/Assets/Scripts/Pills.cs
@@ -0,0 +1,9 @@
+﻿ using UnityEngine;
+using System.Collections;
+
+public class Pills : MonoBehaviour {
+	public float speed;
+	void FixedUpdate () {
+		gameObject.transform.Rotate (Vector3.forward*speed);
+	}
+}
